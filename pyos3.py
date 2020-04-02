2fa7edc7fa5051c1b445a4b040803e1e8d5ee908
Alviner <alvinera@yandex.ru>
2020-04-02 10:45:02 +0300

    [feat] Task Exit

---
+++ pyos3.py
@@ -0,0 +1,67 @@
+"""Python Operating System."""
+from queue import Queue
+
+
+class Task:
+    """An object that runs operating system."""
+
+    taskid = 0
+
+    def __init__(self, target):
+        Task.taskid += 1
+        self.tid = Task.taskid
+        self.target = target
+        self.sendval = None
+
+    def run(self):
+        return self.target.send(self.sendval)
+
+
+class Scheduler:
+    """OS scheduler for task seitching."""
+
+    def __init__(self):
+        self.ready = Queue()
+        self.taskmap = {}
+
+    def new(self, target):
+        newtask = Task(target)
+        self.taskmap[newtask.tid] = newtask
+        self.schedule(newtask)
+        return newtask.tid
+
+    def schedule(self, task):
+        self.ready.put(task)
+
+    def exit(self, task):
+        print(f"Task {task.tid} terminated")
+        del self.taskmap[task.tid]
+
+    def mainloop(self):
+        while self.taskmap:
+            task = self.ready.get()
+            try:
+                result = task.run()
+            except StopIteration:
+                self.exit(task)
+                continue
+            self.schedule(task)
+
+
+def foo():
+    for _ in range(10):
+        print("I'm foo")
+        yield
+
+
+def bar():
+    for _ in range(5):
+        print("I'm bar")
+        yield
+
+
+if __name__ == "__main__":
+    sched = Scheduler()
+    sched.new(foo())
+    sched.new(bar())
+    sched.mainloop()
