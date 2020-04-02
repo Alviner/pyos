914966412be2224984818785daacc21c501ace7c
Alviner <alvinera@yandex.ru>
2020-04-02 13:32:27 +0300

    [feat] Task Waiting

---
+++ pyos6.py
@@ -0,0 +1,133 @@
+"""Python Operating System."""
+from abc import ABC, abstractmethod
+from queue import Queue
+
+
+class Task:
+    """An object that runs operating system."""
+
+    taskid = 0
+
+    def __init__(self, target):
+        Task.taskid += 1
+        self.tid = Task.taskid
+        self.target = target
+        self.sendval = None
+
+    def run(self):
+        return self.target.send(self.sendval)
+
+
+class SystemCall(ABC):
+    @abstractmethod
+    def handle(self):
+        raise NotImplementedError
+
+
+class GetTid(SystemCall):
+    def handle(self):
+        self.task.sendval = self.task.tid
+        self.sched.schedule(self.task)
+
+
+class NewTask(SystemCall):
+    def __init__(self, target):
+        self.target = target
+
+    def handle(self):
+        tid = self.sched.new(self.target)
+        self.task.sendval = tid
+        self.sched.schedule(self.task)
+
+
+class KillTask(SystemCall):
+    def __init__(self, tid):
+        self.tid = tid
+
+    def handle(self):
+        task = self.sched.taskmap.get(self.tid)
+        if task:
+            task.target.close()
+            self.task.sendval = True
+        else:
+            self.task.sendval = False
+        self.sched.schedule(self.task)
+
+
+class WaitTask(SystemCall):
+    def __init__(self, tid):
+        self.tid = tid
+
+    def handle(self):
+        result = self.sched.waitforexit(self.task, self.tid)
+        self.task.sendval = result
+        # If waiting for a non-existent task,
+        # return emmidiately without waiting
+        if not result:
+            self.sched.schedule(self.task)
+
+
+class Scheduler:
+    """OS scheduler for task seitching."""
+
+    def __init__(self):
+        self.ready = Queue()
+        self.taskmap = {}
+        self.exit_waiting = {}
+
+    def new(self, target):
+        newtask = Task(target)
+        self.taskmap[newtask.tid] = newtask
+        self.schedule(newtask)
+        return newtask.tid
+
+    def exit(self, task):
+        print(f"Task {task.tid} terminated")
+        del self.taskmap[task.tid]
+        # Notify other tasks waiting for exit
+        for task in self.exit_waiting.pop(task.tid, {}):
+            self.schedule(task)
+
+    def waitforexit(self, task, waittid):
+        if waittid in self.taskmap:
+            self.exit_waiting.setdefault(waittid, []).append(task)
+            return True
+        return False
+
+    def schedule(self, task):
+        self.ready.put(task)
+
+    def mainloop(self):
+        while self.taskmap:
+            task = self.ready.get()
+            try:
+                result = task.run()
+                if isinstance(result, SystemCall):
+                    result.task = task
+                    result.sched = self
+                    result.handle()
+                    continue
+            except StopIteration:
+                self.exit(task)
+                continue
+            self.schedule(task)
+
+
+def foo():
+    myid = yield GetTid()
+    for i in range(5):
+        print(f"I'm foo {myid}")
+        yield
+
+
+def main():
+    child = yield NewTask(foo())
+    print("Waiting for child")
+    yield WaitTask(child)
+    print("Child done")
+
+
+if __name__ == "__main__":
+    sched = Scheduler()
+    sched.new(main())
+    sched.mainloop()
